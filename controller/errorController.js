9671c73db837d3c95fa32451ed9718586de8a005
Farid <faridforward222@gmail.com>
2023-12-21 01:00:30 +0330

    changes in error Controller .js

--- controller/errorController.js
+++ controller/errorController.js
@@ -61,6 +61,7 @@ const sendErrorProd = (err, req, res) => {
     return res.status(500).json({
       status: "error",
       message: "Something went very wrong!",
+      error: err,
     });
   }
 
