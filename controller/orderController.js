b22c17a695ad00282f8179d6eb3c3b2fdbef8e4d
Farid <faridforward222@gmail.com>
2024-01-07 11:29:34 +0330

    changedsort in getAllOrderrs

--- controller/orderController.js
+++ controller/orderController.js
@@ -39,7 +39,7 @@ exports.getAllOrders = catchAsync(async (req, res, next) => {
   }
   // Query orders based on the constructed query
   const orders = await Order.find(query)
-    .sort({ createdAt: -1 })
+    .sort({ createdAt: 1 })
     .populate("user");
 
   res.status(200).json({
